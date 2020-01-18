d779cde511ae018a8fc551c7499a12f700af7734
YSRKEN <ysr.ken@gmail.com>
2020-01-18 22:50:52 +0900

    [add] Boostrapを導入した

--- client/dcw-client/src/App.tsx
+++ client/dcw-client/src/App.tsx
@@ -1,24 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import 'App.css';
 
 const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="row">
+        <div className="col my-3">
+          <h1 className="text-center">カメラバカ</h1>
+        </div>
+      </div>
     </div>
   );
 }
